a0de298cd24c15aa66587f37e13c62561129b115
Игорь Луковец <igor.lukavets@gmail.com>
2021-11-16 18:15:07 +0300

    1

--- homework.py
+++ homework.py
@@ -38,9 +38,7 @@ last_message = ''
 
 
 def send_message(bot, message):
-    """
-    Отправка сообщений в чат пользователя
-    """
+    """Отправка сообщений в чат пользователя."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logger.info(f'Удачная отправка сообщения: {message}')
@@ -49,9 +47,7 @@ def send_message(bot, message):
 
 
 def get_api_answer(current_timestamp):
-    """
-    Получение ответа от API  приведение его к типам данных Python
-    """
+    """Получение ответа от API  приведение его к типам данных Python."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     headers = {
@@ -80,7 +76,7 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
-    """Проверка корректности ответа API"""
+    """Проверка корректности ответа API."""
     if type(response) is not dict:
         raise ErrType('Ответ API не приведён к типу dict')
     else:
@@ -97,7 +93,7 @@ def check_response(response):
 
 
 def parse_status(homework):
-    """Парсинг ответа API и формирование сообщения для пользователя"""
+    """Парсинг ответа API и формирование сообщения для пользователя."""
     homework_name = homework.get('homework_name')
     if not homework_name:
         logger.error('Отсутствие ключа "homework_name" в ответе API')
@@ -123,7 +119,7 @@ def parse_status(homework):
 
 
 def check_tokens():
-    """Проверка переменных среды"""
+    """Проверка переменных среды."""
     return bool(PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID)
 
 
